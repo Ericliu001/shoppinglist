9397035998a38c4d591ac18d27528323f832318f
Eric Liu <eric.liu@uber.com>
2021-12-29 15:29:34 -0800

    it's ok, delete does not work

--- src/commonMain/kotlin/ShoppingListItem.kt
+++ src/commonMain/kotlin/ShoppingListItem.kt
@@ -3,7 +3,7 @@ import kotlin.random.Random
 
 @Serializable
 data class ShoppingListItem(val desc: String, val priority: Int) {
-    val id: Int = Random.nextInt()
+    val id: Int = desc.hashCode()
 
     companion object {
         const val path = "/shoppingList"
--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,17 +1,11 @@
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import react.Props
-import react.dom.h1
-import react.dom.html.ReactHTML.input
-import react.dom.li
-import react.dom.ul
-import react.functionComponent
-import react.useEffectOnce
-import react.useState
+import react.*
+import react.dom.*
+import kotlinx.html.js.*
+import kotlinx.coroutines.*
 
 private val scope = MainScope()
 
-val app = functionComponent<Props> {_ ->
+val app = fc<Props> {
     var shoppingList by useState(emptyList<ShoppingListItem>())
 
     useEffectOnce {
@@ -23,16 +17,20 @@ val app = functionComponent<Props> {_ ->
     h1 {
         +"Full-Stack Shopping List"
     }
-
     ul {
         shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
             li {
                 key = item.toString()
-                +"[${item.priority}] ${item.desc}"
+                attrs.onClickFunction = {
+                    scope.launch {
+                        deleteShoppingListItem(item)
+                        shoppingList = getShoppingList()
+                    }
+                }
+                +"[${item.priority}] ${item.desc} "
             }
         }
     }
-
     child(inputComponent) {
         attrs.onSubmit = { input ->
             val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
@@ -41,4 +39,5 @@ val app = functionComponent<Props> {_ ->
                 shoppingList = getShoppingList()
             }
         }
-    }}
+    }
+}
