16ad5f6ef273fd808c3100f428b4509ff4ef1ed6
Eric Liu <eric.liu@uber.com>
2021-12-29 14:36:05 -0800

    frontend displays list

---
+++ src/jsMain/kotlin/App.kt
@@ -0,0 +1,34 @@
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import react.Props
+import react.dom.h1
+import react.dom.li
+import react.dom.ul
+import react.functionComponent
+import react.useEffectOnce
+import react.useState
+
+private val scope = MainScope()
+
+val app = functionComponent<Props> {_ ->
+    var shoppingList by useState(emptyList<ShoppingListItem>())
+
+    useEffectOnce {
+        scope.launch {
+            shoppingList = getShoppingList()
+        }
+    }
+
+    h1 {
+        +"Full-Stack Shopping List"
+    }
+
+    ul {
+        shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
+            li {
+                key = item.toString()
+                +"[${item.priority}] ${item.desc}"
+            }
+        }
+    }
+}
